5269ac19360016dcaeb41e5628b9aa2e92f6dce8
krb3p <ryankatherineburke@gmail.com>
2017-04-14 13:16:06 -0400

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    debugger
+    this.props.store.dispatch({
+      type: "DELETE_TODO",
+      id: this.props.id
+    })
+  }
+
   render() {
     return (
       <li>
         {this.props.text}
+        <button onClick={this.handleClick}>Delete</button>
       </li>
     );
   }
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -5,9 +5,13 @@ export default function manageTodo(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_TODO':
+
       id++;
       const todo = Object.assign({}, action.todo, { id: id });
       return { todos: state.todos.concat(todo) };
+    case 'DELETE_TODO':
+      const todos = state.todos.filter(todo => todo.id !== action.id)
+      return {todos}
     default:
       return state;
   }
